4126aad11c16dc30f77f81b8092b1edb7abaa47d
jashanpreet20 <jashankot20>
2023-01-26 14:08:57 +0530

    changes in main js

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-// console.log(document.URL);
-// console.log(document.domain)
-console.log(document.all);
-console.log(document.head);
-console.log(document.images);
-console.log(document.scripts);
- var header=document.getElementById('header-title');
-// header.innerHTML='itmes';
-//header.textContent='make first';
-//header.innerHTML=<h1>hello world</h1>
-//header.style.borderBottom='solid 3px #000'
-console.log(header);
-var items=document.getElementById('items');
-items.style.fontWeight='bold';
-items.style.color='green';
-console.log(items);
+// var items=document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor='green';
+
+// for(var i=0;i<items.length;i++)
+// {
+//     items[i].style.fontWeight='bold';
+// }
+
+
+// console.log(item);
+// console.log(items);
+
+var li=document.getElementsByTagName('li');
+li[2].style.backgroundColor='yellow';
+
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.color='green';
+}
+
+
+
+console.log(li);
+
+
+
