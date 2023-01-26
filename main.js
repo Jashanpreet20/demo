76b91e44306e5e4f9a4698fe85c01422cdba8145
jashanpreet20 <jashankot20>
2023-01-26 11:29:38 +0530

    main js file

---
+++ main.js
@@ -0,0 +1,16 @@
+// console.log(document.URL);
+// console.log(document.domain)
+console.log(document.all);
+console.log(document.head);
+console.log(document.images);
+console.log(document.scripts);
+ var header=document.getElementById('header-title');
+// header.innerHTML='itmes';
+//header.textContent='make first';
+//header.innerHTML=<h1>hello world</h1>
+//header.style.borderBottom='solid 3px #000'
+console.log(header);
+var items=document.getElementById('items');
+items.style.fontWeight='bold';
+items.style.color='green';
+console.log(items);
