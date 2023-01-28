851399840664d2080f0ee64f1fff151c58589d9b
jashanpreet20 <jashankot20>
2023-01-28 18:37:21 +0530

    register js file delete nodes

--- register.js
+++ register.js
@@ -2,44 +2,84 @@ const form=document.querySelector('#my-form');
 const nameInput=document.querySelector('#name');
 const emailInput=document.querySelector('#email');
 const msg=document.querySelector('.msg');
-const userlist=document.querySelector('#users');
-
+const userlist=document.getElementById('users');
 
 
 form.addEventListener('submit',onsubmit);
 
+userlist.addEventListener('click',removeitem);
+
+
+
+
 function onsubmit(e){
     e.preventDefault();
-    var showlist=[];
-   var name=nameInput.value;
-   var email=emailInput.value;
-    var myobj ={
-       name,
-       email
-    }
+    
+    
+var showlist=[];
+var names=nameInput.value;
+var email=emailInput.value;
+ var myobj ={
+    names,
+    email
+ }
+
     showlist.push(myobj);
-    localStorage.setItem(myobj.name,JSON.stringify(showlist));
+    localStorage.setItem(myobj.names,JSON.stringify(showlist));
     // localStorage.setItem('name',nameInput.value);
     // localStorage.setItem('email',emailInput.value);
      
+    //create li
+var li=document.createElement('li');
+
+//create button
+var button=document.createElement('button');
+button.appendChild(document.createTextNode('delete'));
+li.appendChild( document.createTextNode( '\u00A0' ) );
+
+
     if(nameInput.value ==='' || emailInput.value === ''){
         msg.classList.add('error');
         msg.innerHTML='please enter all field';
 
         setTimeout(() => msg.remove(),3000);
     }else{
-        var li=document.createElement('li');
+         li=document.createElement('li');
         li.appendChild(document.createTextNode(`${nameInput.value} : 
         ${emailInput.value}`));
         
-        userlist.appendChild(li);
+        
+
+       
+
+        li.appendChild(button);
+         userlist.appendChild(li);
 
         //clear the fields
 
         nameInput.value='';
         emailInput.value='';
 
+        button.onclick = () =>{
+            localStorage.removeItem(myobj.names);
+            userlist.removeChild(li);
+           }
+
+
     }
+
+
+     //create dlt button
+     
 }
+
+function removeitem(e)
+{
+   
+  
+   
+}
+
+
 
 
